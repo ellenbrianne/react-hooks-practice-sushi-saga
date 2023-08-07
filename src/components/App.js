b6358a0e0a058df01cad91ef4c0262501f01eaf2
Ellen Bennett <ellenbennett@Ellens-MacBook-Air.local>
2023-08-07 02:07:07 -0600

    fetching all sushis, only rendering 4 to the page

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushis, setSushis] = useState([]);
+
+  useEffect(() => {
+    fetch(API)
+    .then(r => r.json())
+    .then(sushis => {
+      const randomize = array => {
+        for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          const temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+        };
+      };
+      randomize(sushis);
+      setSushis(sushis);
+    });
+  }, [])
+
+  const fourSushi = sushis.slice(0, 4);
+
   return (
     <div className="app">
-      <SushiContainer />
+      <SushiContainer fourSushi={fourSushi}/>
       <Table />
     </div>
   );
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,10 +1,16 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({ fourSushi }) {
+
+  const sushiDisplay = fourSushi.map(sushi => (
+    <Sushi key={sushi.id} sushi={sushi} />
+  ));
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
+      {sushiDisplay}
       <MoreButton />
     </div>
   );
